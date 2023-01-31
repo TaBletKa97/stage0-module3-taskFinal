a87030dc46225e2f3754e5a0d307de9bc2edca87
tabletka97 <mtabletka.by@gmail.com>
2023-01-31 12:18:04 +0300

    finalModuleTask done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        System.out.println(number % 10 + number % 100 / 10
+                + number % 1000 / 100 + number % 10000 / 1000);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        final String DELIMITER = "";
+        System.out.println(DELIMITER + number % 10 + number % 100 / 10
+                + number % 1000 / 100);
     }
 }
